a517ef375c6a7e77f97e36b9969dde37e115d56d
Goodpudding <james.trey.young@gmail.com>
2023-09-06 01:24:06 -0700

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
         <button
           type="button"
-          onClick={() => window.open("https://github.com/goodpudding")}
+          onClick={() => window.open("https://github.com/goodpudding/Ai_Summarizer")}
           className="black_btn"
         >
           Github
